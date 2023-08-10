faee3ecd12f3ce44651a6f2a3b19ffb513e14ffb
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-08-10 12:36:31 -0300

    init

---
+++ src/main/java/com/uniamerica/lembretes/LembretesApplication.java
@@ -0,0 +1,13 @@
+package com.uniamerica.lembretes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LembretesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LembretesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniamerica/lembretes/LembretesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniamerica.lembretes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LembretesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
